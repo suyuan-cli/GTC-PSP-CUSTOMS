49eb07bbfb819d7137aba2b37c0a4d1b2f78f170
hejiawei2017 <499200621@qq.com>
2021-01-13 18:56:37 +0800

    提交ask.js

---
+++ ask.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        type: "confirm",
+        name: "useSuyuanUi",
+        message: "你是否使用溯源ui？"
+    },
+    {
+        type: "input",
+        name: "author",
+        message: "author"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "description"
+    },
+    {
+        type: "input",
+        name: "license",
+        message: "license"
+    }
+]
